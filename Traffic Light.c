fe358d095ec84ef4638a6b266573700fbd7d8dd0
AhmedSamy04 <87011921+AhmedSamy04@users.noreply.github.com>
2023-06-25 20:08:35 +0300

    Add files via upload

---
+++ Traffic Light.c
@@ -0,0 +1,87 @@
+/*
+ * Traffic_Light.c
+ *
+ * Created: 9/14/2022 11:56:37 PM
+ *  Author: Ahmed Sami
+ */ 
+
+#include "LED.h"
+#include "LCD.h"
+#include "timer.h"
+#define F_CPU 8000000UL
+#include <util/delay.h>
+#include <avr/interrupt.h>
+volatile unsigned char counter=0;
+unsigned char counter_green=10;
+unsigned char counter_yellow=5;
+unsigned char counter_red=7;
+
+int main(void)
+{
+	LED_vInit('D',0);
+	LED_vInit('D',1);
+	LED_vInit('D',2);
+	LCD_vInit();
+	timer_CTC_init_interrupt();
+	
+    while(1)
+    {
+         counter_green=10;
+		 counter_yellow=5;
+		 counter_red=7;
+		 LCD_clearscreen();
+		 LCD_vSend_string("Remaining 10 Sec");
+		 LED_vTurnOn('D',0);
+		 while(counter_green>0)
+		 {
+			 if(counter>=100)
+			 {
+				 counter=0;
+				 counter_green--;
+				 LCD_movecursor(1,11);
+				 LCD_vSend_char(' ');
+				 LCD_vSend_char((counter_green%10)+48);
+			 }
+		 }
+		 _delay_ms(500);
+		 LED_vTurnOff('D',0);
+		 LED_vTurnOn('D',1);
+		 LCD_clearscreen();
+		 LCD_vSend_string("Remaining  5 Sec");
+		 while(counter_yellow>0)
+		 {
+			 if(counter>=100)
+			 {
+				 counter=0;
+				 counter_yellow--;
+				 LCD_movecursor(1,11);
+				 LCD_vSend_char(' ');
+				 LCD_vSend_char((counter_yellow%10)+48);
+			 }
+		 }
+		 _delay_ms(500);
+		 LED_vTurnOff('D',1);
+		 LED_vTurnOn('D',2);
+		 LCD_clearscreen();
+		 LCD_vSend_string("Remaining  7 Sec");
+		 while(counter_red>0)
+		 {
+			 if(counter>=100)
+			 {
+				 counter=0;
+				 counter_red--;
+				 LCD_movecursor(1,11);
+				 LCD_vSend_char(' ');
+				 LCD_vSend_char((counter_red%10)+48);
+			 }
+		 }
+		 _delay_ms(500);
+		 LED_vTurnOff('D',2);
+		 
+    }
+}
+
+ISR(TIMER0_COMP_vect)
+{
+	counter++;
+}
