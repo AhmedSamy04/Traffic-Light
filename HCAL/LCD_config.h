500300abfdda94888057533b56fb8bb0df548aa8
AhmedSamy04 <87011921+AhmedSamy04@users.noreply.github.com>
2023-06-25 20:07:37 +0300

    Add files via upload

---
+++ HCAL/LCD_config.h
@@ -0,0 +1,12 @@
+/*
+ * LCD_config.h
+ *
+ * Created: 8/15/2022 12:49:23 AM
+ *  Author: Ahmed Sami
+ */ 
+
+
+#ifndef LCD_CONFIG_H_
+#define LCD_CONFIG_H_
+#define eight_bits_mode
+#endif /* LCD_CONFIG_H_ */
---
+++ MCAL/std_macros.h
@@ -0,0 +1,17 @@
+/*
+ * std_macros.h
+ *
+ * Created: 8/14/2022 9:46:19 PM
+ *  Author: Ahmed Sami
+ */ 
+
+
+#define REGISTER_SIZE 8
+#define SET_BIT(reg,bit)    reg|=(1<<bit)
+#define CLR_BIT(reg,bit)    reg&=(~(1<<bit))
+#define TOG_BIT(reg,bit)    reg^=(1<<bit)
+#define READ_BIT(reg,bit)    (reg&(1<<bit))>>bit
+#define IS_BIT_SET(reg,bit)  (reg&(1<<bit))>>bit
+#define IS_BIT_CLR(reg,bit)  !((reg&(1<<bit))>>bit)
+#define ROR(reg,num)         reg=(reg<<(REGISTER_SIZE-num))|(reg>>(num))
+#define ROL(reg,num)        reg= (reg>>(REGISTER_SIZE-num))|(reg<<(num))
